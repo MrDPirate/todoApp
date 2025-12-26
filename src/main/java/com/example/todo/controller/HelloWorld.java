7ab03a456a61139acfc718e3d45f258e04881fa2
MrDPirate <khalil.ak.bh1170@gmail.com>
2025-12-26 22:31:39 +0300

    better implemintation of step one

--- src/main/java/com/example/todo/controller/HelloWorld.java
+++ src/main/java/com/example/todo/controller/HelloWorld.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/hello")
 public class HelloWorld {
-    @GetMapping
+    @GetMapping(path = "/hello")
     public String helloWorld(){
         return "Hello World";
     }
