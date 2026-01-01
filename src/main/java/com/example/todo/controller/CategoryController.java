0a2af76f0d5b1ffd61e05f2e8db15f8d8c91f7a5
MrDPirate <khalil.ak.bh1170@gmail.com>
2026-01-01 21:29:04 +0300

    Read Category

--- src/main/java/com/example/todo/controller/CategoryController.java
+++ src/main/java/com/example/todo/controller/CategoryController.java
@@ -3,10 +3,10 @@ package com.example.todo.controller;
 import com.example.todo.model.Category;
 import com.example.todo.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/")
@@ -26,9 +26,21 @@ public class CategoryController {
     }
     //Read all
 
+    @GetMapping("categories")
+    public List<Category> getCategory(){
+        System.out.println("Controller Calling getCategory - all ==>");
+        return categoryService.getCategory();
+    }
+
     //Read One
+    @GetMapping("categories/{categoryId}")
+    public Optional<Category> getCategory(@PathVariable(value = "categoryId") Long id){
+        System.out.println("Controller Calling getCategory - one ==>");
+        return categoryService.getCategory(id);
+    }
 
     //Update
+
 
     //Delete
 }
--- src/main/java/com/example/todo/service/CategoryService.java
+++ src/main/java/com/example/todo/service/CategoryService.java
@@ -1,10 +1,14 @@
 package com.example.todo.service;
 
 import com.example.todo.exception.InformationExistException;
+import com.example.todo.exception.InformationNotFoundException;
 import com.example.todo.model.Category;
 import com.example.todo.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -15,6 +19,7 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    //Create
     public Category createCategory(Category categoryObject){
         System.out.println("Service Calling createCategory ==>");
         Category category = categoryRepository.findByName(categoryObject.getName());
@@ -26,4 +31,25 @@ public class CategoryService {
                 return categoryRepository.save(categoryObject);
         }
     }
+
+    //Read all
+    public List<Category> getCategory(){
+        System.out.println("Service Calling getCategory ==>");
+        return categoryRepository.findAll();
+    }
+
+    //Read one
+    public Optional<Category> getCategory(Long id){
+        System.out.println("Service Calling getCategory ==>");
+        Optional<Category> category = categoryRepository.findById(id);
+        if (category.isPresent()){
+            return category;
+        }else {
+            throw new InformationNotFoundException("Category with id "+id+" not found");
+        }
+    }
+
+    //Update
+
+    //Delete
 }
